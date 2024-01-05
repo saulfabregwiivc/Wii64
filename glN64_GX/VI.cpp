7cad9fae3137dc07f4e4a3d85e5916b51f842d12
saulfabreg Wii VC Project <saulfabregamboa@outlook.com>
2024-01-05 09:22:58 -0500

    Add Video Interlace detection. Unbreaks games which uses Interlace video.
    Courtesy of FIX94 from Mupen64GC-FIX94 ;)

--- glN64_GX/VI.cpp
+++ glN64_GX/VI.cpp
@@ -83,6 +83,10 @@ void VI_UpdateSize()
 
 	if (VI.width == 0) VI.width = 320;
 	if (VI.height == 0) VI.height = 240;
+
+	// Interlace detection
+	if ((*REG.VI_STATUS>>6)&1)
+		VI.height*=2;
 }
 
 void VI_UpdateScreen()
