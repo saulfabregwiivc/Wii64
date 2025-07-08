50f8ecd8532057e05a2e9a3412e8556518b2d3f4
saulfabreg Wii VC Project <saulfabregamboa@outlook.com>
2025-07-08 13:13:08 -0500

    add CountPerOp setting (without GUI option; editable in settings.cfg or arguments)
    References:
    https://github.com/emukidid/Wii64/pull/21/files
    https://github.com/emukidid/Wii64/commit/575bd20d9e0ae8be585e92cc0a35dbbf1fd895bf

--- r4300/ppc/Wrappers.c
+++ r4300/ppc/Wrappers.c
@@ -51,7 +51,7 @@ static PowerPC_func* last_func;
  *  $sp	    | old sp
  */
 
-inline unsigned int dyna_run(PowerPC_func* func, PowerPC_instr *code){
+inline u32 dyna_run(PowerPC_func* func, PowerPC_instr *code){
 	
 	PowerPC_instr* return_addr;
 
@@ -85,7 +85,7 @@ inline unsigned int dyna_run(PowerPC_func* func, PowerPC_instr *code){
 	last_func = r30;
 	link_branch = link_branch == return_addr ? NULL : link_branch - 1;
 
-	return r3;
+	return (u32)r3;
 }
 
 void dynarec(unsigned int address){
@@ -183,7 +183,7 @@ int dyna_update_count(unsigned int pc, int isDelaySlot){
 #else
 int dyna_update_count(unsigned int pc){
 #endif
-	Count += (pc - r4300.last_pc)/2;
+	Count += ((pc - r4300.last_pc) >> 2) * count_per_op;
 	r4300.last_pc = pc;
 
 #ifdef COMPARE_CORE
